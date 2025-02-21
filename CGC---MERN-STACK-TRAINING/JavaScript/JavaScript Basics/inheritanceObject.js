1c2ac52a150ff07c4118b9815ce1b4c05229f5b0
Abdul Aamir <abdulaamir9496@gmail.com>
2025-02-21 22:45:36 +0530

    Class Object inheritance

--- CGC---MERN-STACK-TRAINING/JavaScript/JavaScript Basics/inheritanceObject.js
+++ CGC---MERN-STACK-TRAINING/JavaScript/JavaScript Basics/inheritanceObject.js
@@ -5,6 +5,14 @@ var Mobile = importedCalss.Mobile;
 class SamsungMobile extends Mobile {
     constructor() {
         super();
+        this.brand = "Android";
+        if(fiveG) {
+            this.is5G = fiveG;
+        } else {
+            throw new Error("Value must be a true");
+            console.log("Value must be true");
+        }
+        console.log("Call from this file", this.brand)
     }
 }
 
