056c8a01b4d481eb3ec0178c403560681e871217
Abdul Aamir <abdulaamir9496@gmail.com>
2025-02-21 22:45:14 +0530

    Array Operations

---
+++ CGC---MERN-STACK-TRAINING/JavaScript/TDC FullStack Assignment No. 5/Arrays and Array Methods/Array Operations.js
@@ -0,0 +1,36 @@
+// Part 2: Arrays and Array Methods
+// Task 2: Array Operations
+// 1. Create an array fruits containing the following values: ['Apple', 'Banana', 'Mango', 'Orange', 'Grapes'] ✅
+
+var fruits = ['Apple', 'Banana', 'Mango', 'Orange', 'Grapes'];
+
+// 2. Perform the following operations:
+
+// Add "Pineapple" to the end of the array.  ✅ 
+fruits.push("Pineapple");
+console.log(fruits);
+
+// Remove the first element from the array.  ✅
+fruits.shift();
+console.log(fruits);
+
+// Insert "Strawberry" at the second position in the array. Find the index of "Mango". ✅
+fruits.splice(1, 0, "Strawberry");
+console.log(fruits);
+
+// Remove "Orange" from the array.  ✅
+fruits.splice(fruits.indexOf("Orange"), 1);
+console.log(fruits);
+
+var mangoIndex = fruits.indexOf("Mango");
+console.log(fruits);
+fruits.splice(fruits.indexOf("Orange"), 1);
+console.log(fruits);
+
+// Sort the array in alphabetical order.  ✅
+fruits.sort();
+console.log(fruits);
+
+// Reverse the order of the array.  ✅
+fruits.reverse();
+console.log(fruits);
