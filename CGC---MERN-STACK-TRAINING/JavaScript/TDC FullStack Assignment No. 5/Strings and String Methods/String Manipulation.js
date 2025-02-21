237e4b6acd002e7ef1e62e7052193ad07d146cd4
Abdul Aamir <abdulaamir9496@gmail.com>
2025-02-21 22:44:12 +0530

    String Manipulation

---
+++ CGC---MERN-STACK-TRAINING/JavaScript/TDC FullStack Assignment No. 5/Strings and String Methods/String Manipulation.js
@@ -0,0 +1,22 @@
+// 1. Declare a string variable sentence with the value:
+// "JavaScript is a powerful and popular programming language."  ✅
+var codingLanguage = "JavaScript is a powerful and popular programming language.";
+
+// Convert the string to uppercase. ✅
+codingLanguage.toUpperCase();
+
+// Convert the string to lowercase. ✅
+codingLanguage.toLowerCase();
+
+//Find the index of the word "popular". ✅
+codingLanguage.findIndex("popular");
+
+//Extract the word "powerful" using slice(). ✅
+codingLanguage.slice(15, 23);
+
+//Replace "JavaScript" with "JS" using replace(). ✅
+codingLanguage.replace("JavaScript", "JS");
+
+//Check if the string contains the word "language" using includes(). ✅
+codingLanguage.includes("language");
+
