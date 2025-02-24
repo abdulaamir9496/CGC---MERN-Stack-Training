6b403903b2b3af52548016947177dd4e1ea59542
Abdul Aamir <abdulaamir9496@gmail.com>
2025-02-24 16:36:57 +0530

    ES6

---
+++ CGC---MERN-STACK-TRAINING/JavaScript/JavaScript Basics/ES6.js
@@ -0,0 +1,40 @@
+//var let & const
+// let age = 10;   //global access
+// // console.log(age);
+
+// {
+//     let age = 20;   //block access
+//     console.log(age);
+// }
+
+// console.log(age);
+
+//function expression
+// function calculate () {};
+//function declaration
+// var add = function() {};
+
+//Arrow Functions:
+// var add = () => {};
+
+// ex: use const as function don't change.
+// const addition = () => 1 + 3;
+// console.log(addition());
+
+//for multi line code we have to write return function
+// const addition1 = () => {
+//     return 5 + 3;
+// }
+// console.log(addition1());
+
+//we don't use 'this' keyword in arrow functions.
+
+
+//Hoisting of a variable: 
+//Hoisting is a default behaviour for variables in JavaScript.
+// var x = 5;
+// console.log(x);
+// var x;
+// console.log(x);
+
+
