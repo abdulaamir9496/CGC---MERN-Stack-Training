982c454e9e14f7e45590c34305b011d00d9750ff
Abdul Aamir <abdulaamir9496@gmail.com>
2025-02-27 15:58:04 +0530

    test

---
+++ CGC---MERN-STACK-TRAINING/JavaScript/JavaScript Basics/test.js
@@ -0,0 +1,28 @@
+const PI = require("./module");
+
+console.log(PI);
+
+// Importing specific functions from the module
+import { add, subtract } from './math.js';
+
+console.log(add(5, 3));       // Output: 8
+console.log(subtract(5, 3));  // Output: 2
+
+// Importing the default export
+import multiply from './math.js';
+
+console.log(multiply(2, 3));  // Output: 6
+
+    // Renaming imported functions
+import { add as sum, subtract as difference } from './math.js';
+
+console.log(sum(5, 3));        // Output: 8
+console.log(difference(5, 3)); // Output: 2
+
+
+
+  // Importing the user object
+import { user } from './user.js';
+
+console.log(user.name);  // Output: Alice
+console.log(user.age);   // Output: 25
