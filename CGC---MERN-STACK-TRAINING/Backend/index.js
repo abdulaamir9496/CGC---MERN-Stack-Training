fddaad2b8b92a193f97f68430c62aaeb0632ab78
Abdul Aamir <abdulaamir9496@gmail.com>
2025-03-12 08:40:47 +0530

    Backend: Express Js

--- CGC---MERN-STACK-TRAINING/Backend/index.js
+++ CGC---MERN-STACK-TRAINING/Backend/index.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
 
+
+//we can also write with end points as shown below:
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('This is home page Welcome to Express.Js world')
 })
 
-app.listen(3000)
+app.get('/about', function (req, res) {
+    res.send('This is about page')
+})
+
+app.get('/contact', function(req, res) {
+    res.send("This is contacts page")
+})
+
+app.listen(5000, () => console.log("Server is running up...!"));
+
+//request goes to through Use : middleware (everytime )
+// app.use((req, res) => {
+//     res.status(200);  //converting ot method
+//     res.send("Welcome to my Express Js Server!");  //we can send any type of data
+// });
+
+//we also do chaingin of mehods.
